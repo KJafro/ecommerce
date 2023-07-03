c4fae821d653051e8cd3c395164ae8f595119671
KJafro <Kurtj222@outlook.com>
2023-07-03 21:34:28 +0100

    Routing issue

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <Contexter>
-    <Router>
+    <Router basename='/ecommerce'>
     <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={loggedIn ? <Home/> : <Login />} />
