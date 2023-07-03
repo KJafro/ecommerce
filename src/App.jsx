ca541a1be267db3cd251ec353f2be397cc115de1
KJafro <Kurtj222@outlook.com>
2023-07-03 21:35:56 +0100

    Routing

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <Contexter>
-    <Router basename='/ecommerce'>
+    <Router basename='/merry-basbousa-c35611'>
     <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/login" element={loggedIn ? <Home/> : <Login />} />
