3f3b35fe31decc61889973b18f5f1efcbf9cb0c8
KJafro <Kurtj222@outlook.com>
2023-07-03 21:48:48 +0100

    Fixed the menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -32,13 +32,13 @@ export const Menu = () => {
               <div className="modal-content">
                 <h1 style={{textAlign: "center"}}>MENU</h1>
                 <hr className='hrModal'/>
-                <Link to='/'><h2>PRODUCTS</h2></Link>
-                <Link to='/orders'><h2>ORDERS</h2></Link>
-                <Link to='/reviews'><h2>REVIEWS</h2></Link>
-                <Link to='/about'><h2>ABOUT</h2></Link>
+                <Link to='/' onClick={toggleModal}><h2>PRODUCTS</h2></Link>
+                <Link to='/orders' onClick={toggleModal}><h2>ORDERS</h2></Link>
+                <Link to='/reviews' onClick={toggleModal}><h2>REVIEWS</h2></Link>
+                <Link to='/about' onClick={toggleModal}><h2>ABOUT</h2></Link>
                 <hr className='hrModal'/>
-                {loggedIn ? <Link to='/profile'><h2>ACCOUNT</h2></Link> : <Link to='/register'><div><h2>REGISTER</h2><h2>LOGIN</h2></div></Link>}
-                <Link to='/cart'><ul className='hamburgerMenuCart'><BsCart size={40}/></ul></Link>
+                {loggedIn ? <Link to='/profile' onClick={toggleModal}><h2>ACCOUNT</h2></Link> : <Link to='/register'><div><h2>REGISTER</h2><h2>LOGIN</h2></div></Link>}
+                <Link to='/cart' onClick={toggleModal}><ul className='hamburgerMenuCart'><BsCart size={40}/></ul></Link>
                 <Link to='/cart'><p className='hamburgerMenuItems'>{values.items.length > 0 && values.items.length}</p></Link>
                 <button className="close-modal" onClick={toggleModal}>
                   X
